bfcd63e2c069fcb6e69e6c67ecec223d6bf3ba08
juan <juan.carlos.vidal@gmail.com>
2026-07-18 22:59:13 +0200

    Data generation scripts for HPC cluster

--- hpc/py_mlp.sh
+++ hpc/py_mlp.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=32G
 # Override with: PYTHON=/path/to/python sbatch py_mlp.sh
-PYTHON=${PYTHON:-/mnt/beegfs/home/juan.vidal/miniconda3/envs/diabetes/bin/python3.10}
+PYTHON=${PYTHON:-/mnt/beegfs/home/juan.vidal/miniforge3/envs/covroc/bin/python3.10}
 
 # Run from the repository root (SLURM's working directory is the submission directory)
 command="$PYTHON src/simulation/mlp_reg_data_simulation_multi.py -i $1 -o $2 -c 6 -f 5 -e 800 -b 64"
--- hpc/py_rfo.sh
+++ hpc/py_rfo.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=32G
 # Override with: PYTHON=/path/to/python sbatch py_rfo.sh
-PYTHON=${PYTHON:-/mnt/beegfs/home/juan.vidal/miniconda3/envs/diabetes/bin/python3.10}
+PYTHON=${PYTHON:-/mnt/beegfs/home/juan.vidal/miniforge3/envs/covroc/bin/python3.10}
 
 # Run from the repository root (SLURM's working directory is the submission directory)
 command="$PYTHON src/simulation/rf_reg_data_simulation_multi_2.py -i $1 -o $2 -c 6 -f 2 -e 800 -b 64"
